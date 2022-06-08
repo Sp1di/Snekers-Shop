34fc6a10a641e3a09d48eda3b0a119ac5d69aca7
Anastasiya Zyazmina <46940546+Sp1di@users.noreply.github.com>
2022-06-08 17:27:51 +0300

    шаблон главной  страницы
     разработал шаблон карточек товаров и шаблон главной  страницы

--- src/App.js
+++ src/App.js
@@ -2,25 +2,85 @@ import React from "react";
 
 function App() {
   return (
-      <div className="wrapper">
-        <header className="headerLeft">
-            <div>
-                <img src="/img/logo.png" />
-                <div className="headerInfo">
-                    <h3>Sneakers Shop</h3>
+      <div className="wrapper clear">
+        <header className="d-flex justify-between align-center">
+            <div className="d-flex align-center">
+                <img width={40} height={40} src="img/logo.png" alt="logo"/>
+                <div >
+                    <h3 className="text-uppercase">Sneakers Shop</h3>
                     <p>Магазин лучших кросовок</p>
                 </div>
             </div>
-            <ul className="headerRight">
-                <li>
-                    <svg/>
+            <ul className="d-flex ">
+                <li className="mr-30">
+                    <img width={18} height={18} src="/img/basket.svg" alt="basket" />
                     <span>1205 руб.</span>
                 </li>
                 <li>
-                    <svg/>
+                    <img width={18} height={18} src="/img/like.svg" alt="like" />
+                </li>
+                <li>
+                    <img width={25} height={25} src="/img/user.svg" alt="user" />
                 </li>
             </ul>
         </header>
+        <div className="content p-50">
+            <h1 className="mb-40">Все кросовки</h1>
+            <div className="sneakers d-flex">
+                <div className="card">
+                    <img width={133} height={112} src="/img/Sneakers/1.jpg" alt="1"/>
+                    <h5>Мужские Кроссовки Nike Air Max 270</h5>
+                    <div className="d-flex justify-between ">
+                        <div className="d-flex flex-column">
+                            <span>Цена:</span>
+                            <strong>11 204 р.</strong>
+                        </div>
+                        <button className="button">
+                            <img width={8} height={8} src="/img/plus.svg" alt="Plus"/>
+                        </button>
+                    </div>
+                </div>
+                <div className="card">
+                    <img width={133} height={112} src="/img/Sneakers/2.jpg" alt="2"/>
+                    <h5>Мужские Кроссовки Nike Air Max 270</h5>
+                    <div className="d-flex justify-between ">
+                        <div className="d-flex flex-column">
+                            <span>Цена:</span>
+                            <strong>11 204 р.</strong>
+                        </div>
+                        <button className="button">
+                            <img width={8} height={8} src="/img/plus.svg" alt="Plus"/>
+                        </button>
+                    </div>
+                </div>
+                <div className="card">
+                    <img width={133} height={112} src="/img/Sneakers/3.jpg" alt="3"/>
+                    <h5>Мужские Кроссовки Nike Air Max 270</h5>
+                    <div className="d-flex justify-between align-center">
+                        <div className="d-flex flex-column ">
+                            <span>Цена:</span>
+                            <strong>11 204 р.</strong>
+                        </div>
+                        <button className="button">
+                            <img width={8} height={8} src="/img/plus.svg" alt="Plus"/>
+                        </button>
+                    </div>
+                </div>
+                <div className="card">
+                    <img width={133} height={112} src="/img/Sneakers/4.jpg" alt="4"/>
+                    <h5>Мужские Кроссовки Nike Air Max 270</h5>
+                    <div className="d-flex justify-between ">
+                        <div className="d-flex flex-column">
+                            <span>Цена:</span>
+                            <strong>11 204 р.</strong>
+                        </div>
+                        <button className="button">
+                            <img width={8} height={8} src="/img/plus.svg" alt="Plus"/>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
       </div>
   );
 }
